a533b6defe43950682ee46f2cbe530e015ecac9b
Daniel Boyle <boylede+github@gmail.com>
2020-02-29 16:38:09 -0800

    add loading of game settings

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,22 @@
+use amethyst::{
+    assets::{
+        Handle, 
+        Asset,
+    },
+    core::ecs::DenseVecStorage,
+};
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Clone, Debug, Default, PartialEq, Eq, Deserialize, Serialize)]
 pub struct GameSettings {
     width: u32,
     height: u32,
     area_size: u32,
+}
+
+impl Asset for GameSettings {
+    const NAME: &'static str = "hexadie::GameSettings";
+    type Data = Self;
+    type HandleStorage = DenseVecStorage<Handle<GameSettings>>;
 }
