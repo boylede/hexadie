f53f377c023bb39ce8d2ee669f1e23813c56ec21
Daniel Boyle <boylede+github@gmail.com>
2020-04-04 12:13:47 -0700

    place widely used asset handles in a resource

---
+++ src/assets.rs
@@ -0,0 +1,25 @@
+use amethyst::{
+    DataDispose, DataInit, GameDataBuilder,
+    assets::{
+        Asset, Format,
+        AssetStorage, Loader, ProgressCounter, Handle, Progress, Completion, RonFormat, Tracker,
+    },
+    core::{ecs::Entity},
+    input::{is_close_requested, is_key_down, VirtualKeyCode},
+    prelude::*,
+    renderer::{ImageFormat, SpriteSheet, SpriteSheetFormat, Texture, Mesh, formats::mesh::ObjFormat},
+    window::ScreenDimensions,
+    ui::{TtfFormat,  FontAsset},
+    ecs::prelude::Dispatcher,
+};
+
+use crate::config::GameSettings;
+
+#[derive(Clone)]
+pub struct HexAssets {
+    pub spritesheet: Handle<SpriteSheet>,
+    pub font: Handle<FontAsset>,
+    pub settings: Handle<GameSettings>,
+    pub hex_sprites: Handle<SpriteSheet>,
+}
+
