1b73cc02ef9a2a01c14d336e600060bbe0b3b65a
Daniel Boyle <boylede+github@gmail.com>
2020-04-04 20:06:11 -0700

    beginings of map generation

--- src/map_selection/mod.rs
+++ src/map_selection/mod.rs
@@ -2,11 +2,23 @@ use amethyst::{
     assets::{
         Handle,
         Loader,
+        AssetStorage,
     },
-    core::{ecs::{Entity, DenseVecStorage, storage::UnprotectedStorage}, transform::{Transform}},
+    core::{
+        ecs::{
+            Entity,
+            DenseVecStorage,
+            Component,
+            Entities,
+            WriteStorage
+        },
+        transform::{
+            Transform
+        }
+    },
     input::{get_key, is_close_requested, is_key_down, VirtualKeyCode},
     prelude::*,
-    renderer::{SpriteSheet, Mesh, MaterialDefaults, Material},
+    renderer::{SpriteSheet, Mesh, MaterialDefaults, Material, SpriteRender, Transparent},
     window::ScreenDimensions,
     ui::{Anchor, TtfFormat, UiText, UiTransform, FontAsset, UiButtonBuilder, Interactable, UiEvent},
 };
@@ -23,7 +35,11 @@ impl SimpleState for MapSelectionState {
         let mut world = data.world;
         let _dimensions = (*world.read_resource::<ScreenDimensions>()).clone();
         let assets = (*world.read_resource::<HexAssets>()).clone();
+
+        world.register::<Hexagon>();
+
         create_title_text(world, &assets.font, "Map Selection");
+        create_map(world);
     }
 
     fn handle_event(
@@ -89,4 +105,112 @@ fn create_title_text(world: &mut World, font: &Handle<FontAsset>, text: &str) ->
         )).build();
 
         text
+}
+
+fn create_map(world: &mut World) {
+
+    let assets = (*world.read_resource::<HexAssets>()).clone();
+
+    let mut settings: GameSettings = {
+        let handle = &assets.settings;
+        let asset_storage =  world.read_resource::<AssetStorage<GameSettings>>();
+        asset_storage.get(handle).expect("failed to load settings").clone()
+    };
+
+    let player_count = settings.player_count.unwrap_or(8);
+    let territory_count = player_count * 4;
+
+    let width = settings.width;
+    let height = settings.height;
+
+    let area_size = if settings.area_size == 0 { 1 } else {settings.area_size};
+
+    let hex_count = territory_count * settings.area_size;
+    let overall_area = width * height;
+
+    let hex_offset_y = 56.0;
+    let hex_offset_x = 49.0;
+    let scale = 1.0;
+
+
+
+    println!("making map {}x{} = {}, with {} hexes in {} territories", width, height, overall_area, hex_count, territory_count);
+
+    let mut hex_list = vec![];
+    // world.exec(|(mut entities, mut hexagons): (Entities, WriteStorage<Hexagon>)| {
+    for x in 0..width {
+        for y in 0..height {
+            let even_column_offset: (f32, f32) = if x % 2 == 0 {
+                (-0.5, 28.0)
+            } else {
+                (0.0, 0.0)
+            };
+            let x = x as f32;
+            let y = y as f32;
+            
+            // entities.build_entity()
+            let sprite_sheet_handle = assets.hex_sprites.clone();
+
+            let sprite_render = SpriteRender {
+                sprite_sheet: sprite_sheet_handle,
+                sprite_number: 0,
+            };
+            let mut transform = Transform::default();
+            transform.set_translation_xyz(x * hex_offset_x + even_column_offset.0, y * hex_offset_y + even_column_offset.1, 0.0);
+            transform.set_scale([scale, scale, scale].into());
+            // transform.set_rotation_2d(rotation / 180.0 * PI);
+
+            let hexagon = Hexagon {
+                x: x,
+                y: y,
+                init: None,
+            };
+
+            let mut sprite_builder = world
+                .create_entity()
+                .with(sprite_render)
+                .with(transform)
+                .with(hexagon)
+                .with(Transparent);
+            // if let Some((r, g, b)) = tint {
+            //     sprite_builder = sprite_builder.with(Tint(Srgb::new(r, g, b).into()));
+            // }
+            let hex = sprite_builder.build();
+            hex_list.push(hex);
+        }
+    }        
+    // });
+
+    
+
+
+
+}
+
+struct Hexagon {
+    x: f32,
+    y: f32,
+    init: Option<InitializedHexagon>,
+}
+
+struct InitializedHexagon {
+    player: Player,
+    neighbors: [Entity; 6],
+    center: Entity,
+}
+
+
+impl Component for Hexagon {
+    type Storage = DenseVecStorage<Self>;
+}
+
+enum Player {
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
 }
